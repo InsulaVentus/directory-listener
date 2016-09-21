86a83557e2b4f3beff2996732c243e44a7bd8bbf
InsulaVentus <insula.ventus@gmail.com>
2016-09-21 08:22:43 +0200

    Updated test to match what it's testing.

--- src/test/scala/listenable/ComponentTest.scala
+++ src/test/scala/listenable/ComponentTest.scala
@@ -1,27 +1,29 @@
+package listenable
+
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSuite, Matchers}
 
 @RunWith(classOf[JUnitRunner])
-class DirectoryListenerTest extends FunSuite with Matchers {
+class ComponentTest extends FunSuite with Matchers {
 
   test("should return false when directory name is empty") {
-    DirectoryListener.isValidDirectoryName("") shouldBe false
+    Component.isValidDirectoryName("") shouldBe false
   }
 
   test("should return true when a valid directory name is passed") {
-    DirectoryListener.isValidDirectoryName("35465.445") shouldBe true
+    Component.isValidDirectoryName("35465.445") shouldBe true
   }
 
   test("should return false when valid directory name ending with '.'") {
-    DirectoryListener.isValidDirectoryName("321321.") shouldBe false
+    Component.isValidDirectoryName("321321.") shouldBe false
   }
 
   test("should return false when directory name starts with '.'") {
-    DirectoryListener.isValidDirectoryName(".3.4") shouldBe false
+    Component.isValidDirectoryName(".3.4") shouldBe false
   }
 
   test("should return false when directory name contains letters") {
-    DirectoryListener.isValidDirectoryName("1.E.4") shouldBe false
+    Component.isValidDirectoryName("1.E.4") shouldBe false
   }
 }
